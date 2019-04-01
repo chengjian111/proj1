5bd0ac9419cc82d13baa12d4c588209b1f227a95
chengjian111 <49147063+chengjian111@users.noreply.github.com>
2019-04-01 20:10:36 +0800

    Add files via upload

---
+++ HelloWord.java
@@ -0,0 +1,13 @@
+
+public class HelloWord {
+
+	
+	public static void main(String[] args) {
+		
+	}
+	public void syso() {
+		// TODO Auto-generated method stub
+	
+		System.out.print("Hello Word!");
+	}
+}
